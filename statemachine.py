a0434be9930a7dafe95da3965b94192b74fb6f17
qiaoyinbo <breakerjoe@163.com>
2024-12-27 00:06:32 +0800

    simple code for state machine

--- statemachine.py
+++ statemachine.py
@@ -1,5 +1,21 @@
+from states.basestate import BaseState
 class StateMachine():
     def __init__(self):
         self.states = {}
         self.transitions = {}
         self.curState = None
+        self.target = None #target mark, such as "A2"
+
+    def change_state(self, state: BaseState):
+        pass
+
+    def drive(self, args):
+        """send drive command to the car
+
+        Args:
+            args (_type_): _description_
+        """
+        pass
+
+#Example
+#state_machine = StateMachine()
